21f99bbe8a56e364356f27b19705bc4126322801
wangzhilin <1032570740@qq.com>
2018-06-03 01:40:55 +0800

    lecture1

--- StanfordTF/lecture1.py
+++ StanfordTF/lecture1.py
@@ -1,1 +1,59 @@
 import tensorflow as tf
+#
+# a = tf.add(2,3)
+#
+# # sess=tf.Session()
+# # print(sess.run(a))
+# # sess.close()
+# with tf.Session() as sess:#auto close!
+#     print(sess.run(a))
+# x=2;y=3
+# op1=tf.add(x,y)
+# op2=tf.multiply(x,y)
+# useless=tf.add(op1,x)
+# op3=tf.pow(op2,op1)
+# with tf.Session() as sess:
+#     op3=sess.run(op3)
+# print(op3)
+# print(useless)
+
+
+#
+# #to add operators to a graph ,set it as default
+# g=tf.Graph()
+# with g.as_default():
+#     x=tf.add(3,5)
+#
+# sess=tf.Session(graph=g)
+# sess.run(x)
+# sess.close()
+#
+# # with tf.Session() as sess:
+# #     sess.run(x)
+#
+# # #to handle the default graph
+# # g=tf.get_default_graph()
+
+
+# #Do not mix default graph and user created graphs
+# g=tf.Graph()
+# #add ops to the default graph
+# a=tf.constant(3)
+# #add ops to the user created graph
+# with g.as_default():
+#     b=tf.constant(5)
+
+
+
+# #Do not mix default graph and user created graphs
+# g1=tf.get_default_graph()
+# g2=tf.Graph()
+# #add ops to the default graph
+# with g1.as_default():
+#     a=tf.constant(2)
+# #add ops to user created graph
+# with g2.as_default():
+#     b=tf.constant(5)
+
+
+
---
+++ tensorfly/NumRecQAQ/network.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist=input_data.read_data_sets("MNIST_data",one_hot=True)
+
+sess=tf.InteractiveSession()
+
+def weight_variable(shape):
+    initial=tf.truncated_normal(shape,stddev=0.1)
+    return tf.Variable(initial)
+def bias_variable(shape):
+    initial=tf.constant(0.1,shape=shape)
+    return tf.Variable(initial)
+def con2d(x,W):
+    return tf.nn.conv2d(x,W,strides=[1,1,1,1],padding="SAME")
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x,ksize=[1,2,2,1],strides=[1,2,2,1],padding="SAME")
+
+W_conv1=weight_variable([5,5,1,32])
+b_conv1=bias_variable([32])
+
+
+x_image=tf.reshape(x,[-1,28,28,1])
+
+h_conv1
+
+#占位符
+x =tf.placeholder('float32',shape=[None,784])
+y_=tf.placeholder('float32',shape=[None,10])#每一行是10维的one-hot
+
+#变量 变量需要session初始化
+W=tf.Variable(tf.zeros([784,10]))
+b=tf.Variable(tf.zeros([10]))
+
+y=tf.nn.softmax(tf.maxmul(x,W)+b)
+cross_entropy=-tf.reduce_sum(y_*tf.log(y))
+
+
+train_step=tf.train.GradientDescentOpitimizer(0.01).minimize(cross_entropy)
+
+
+
+sess.run(tf.global_variables_initializer())
+
+for i in range(100):
+    batch=mnist.train.next_batch(50)
+    train_step.run(feed_dict={x:batch[0],y_:batch[1]})
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(_y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float32'))
+print(accuracy.eval(feed_dict={x:mnist.test.images,y_:mnist.test.labels}))
